f3d0a607aad18030ee39138cb0a1a2e79035968f
marinsomnia <118461086+marinsomnia@users.noreply.github.com>
2024-01-29 16:03:56 +0300

    hw7

---
+++ lesson07HW01/Models/FriendsModel.swift
@@ -0,0 +1,26 @@
+struct FriendsModel: Codable {
+    let response: Response
+    
+    struct Response: Codable {
+        let count: Int
+        let items: [Friend]
+        
+        struct Friend: Codable {
+            let id: Int
+            let firstName: String
+            let lastName: String
+            let online: Int
+            let photo: String
+            let photo200: String
+            
+            private enum CodingKeys: String, CodingKey {
+                case id
+                case firstName = "first_name"
+                case lastName = "last_name"
+                case online
+                case photo = "photo_50"
+                case photo200 = "photo_200_orig"
+            }
+        }
+    }
+}
